deb6453051766fe37094881f7f4a07e6321340a1
Sakhiur2022 <sakhiur.rahman@northsouth.edu>
2026-01-21 20:45:44 +0600

    feat: Implement QR code generation and URL input prompt

---
+++ index.js
@@ -0,0 +1,29 @@
+import fs from 'fs';
+import inquirer from 'inquirer';
+import qr from 'qr-image';
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {
+        type:"input",
+        name:"url",
+        message:""
+    }
+
+  ])
+  .then((answers) => {
+
+let qr_svg = qr.image(answers.url, { type: 'svg' });
+qr_svg.pipe(fs.createWriteStream(`${answers.url}.svg`));
+fs.writeFile('url.txt',answers.url,(error)=>{
+    if(error) console.log(error)
+})
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+        console.log("Prompt couldn't be rendered in the current environment");
+    
+    } else {
+      console.error(error.message)
+    }
+  });
